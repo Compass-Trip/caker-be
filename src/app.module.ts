f86a9ca69e39faa30697297de30b87e14ad55476
eunkang-choi <dmsrkd1216@gmail.com>
2025-10-05 02:22:13 +0900

    :sparkles: Init: 메인 도메인모듈생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './auth/auth.module';
+import { StoresModule } from './stores/stores.module';
+import { ProductsModule } from './products/products.module';
+import { LikesModule } from './likes/likes.module';
+import { CartsModule } from './carts/carts.module';
+import { OrdersModule } from './orders/orders.module';
+import { CustomServicesModule } from './custom-services/custom-services.module';
 
 @Module({
-  imports: [UsersModule, CommonModule, AuthModule, TerminusModule],
+  imports: [
+    UsersModule,
+    CommonModule,
+    AuthModule,
+    TerminusModule,
+    StoresModule,
+    ProductsModule,
+    LikesModule,
+    CartsModule,
+    OrdersModule,
+    CustomServicesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
