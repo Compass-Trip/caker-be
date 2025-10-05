5216f88289d680eea15f840ff6d8dff465c81d19
eunkang-choi <dmsrkd1216@gmail.com>
2025-10-06 08:24:22 +0900

    :memo: Add: swagger모듈설치 및 swagger생성

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,6 +32,8 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/unbound-method': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/no-unsafe-return': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
     },
   },
 );
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/http-exception/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // CORS 활성화
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
 
   // Graceful Shutdown 활성화 (ECS 종료시그널 처리)
   app.enableShutdownHooks();
@@ -14,6 +20,16 @@ async function bootstrap() {
   // 전역 필터 등록
   app.useGlobalFilters(new HttpExceptionFilter());
 
+  // Swagger 설정
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('caker-backend')
+    .setVersion('0.1')
+    .build();
+  const swagger = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, swagger);
+
+  // API Prefix 설정
+  app.setGlobalPrefix('api');
   // 포트 설정
   const port = process.env.PORT || 3000;
 
