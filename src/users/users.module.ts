a4ab343bda95de271d295a0d682aa60eebe0793e
eunkang-choi <dmsrkd1216@gmail.com>
2025-10-06 09:09:06 +0900

    :truck: Chore: user 프로바이더 이관

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './presentation/users.controller';
-import { UsersService } from './domain/service/users.service';
+import { UsersService } from './domain/users.service';
 
 @Module({ controllers: [UsersController], providers: [UsersService] })
 export class UsersModule {}
